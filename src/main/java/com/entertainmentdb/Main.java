6ee99113e974ccba2b6f37821350f259a820f103
alfita96 <alfita96@gmail.com>
2018-02-15 14:15:46 +0100

    Demo

--- src/main/java/com/entertainmentdb/Main.java
+++ src/main/java/com/entertainmentdb/Main.java
@@ -16,6 +16,11 @@
 
 package com.entertainmentdb;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -83,6 +88,14 @@ public class Main {
       config.setJdbcUrl(dbUrl);
       return new HikariDataSource(config);
     }
+  }
+  
+  @RequestMapping("/hello")
+  String hello(Map<String, Object> model) {
+	  RelativisticModel.select();
+	  Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+	  model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+	  return "hello";
   }
 
 }
